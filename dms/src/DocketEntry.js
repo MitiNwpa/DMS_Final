85ee3f4525d5a118f3d681f34d555f376a0ab361
HrishiGulhane <gulhane.hrishikesh@gmail.com>
2019-02-04 17:29:29 +1100

    2

--- dms/src/DocketEntry.js
+++ dms/src/DocketEntry.js
@@ -16,58 +16,108 @@ class DocketEntry extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            items: [],
-            activity: []
+            items: {},
+            activity: {}
         }
-        this.readUser=this.readUser.bind(this);
-        this.readActivity=this.readActivity.bind(this);
+        this.readUser = this.readUser.bind(this);
+        this.readActivity = this.readActivity.bind(this);
+        this.wellwell = this.wellwell.bind(this);
+
 
     }
 
 
-    componentWillMount(){
-        // this.readUser();
+    componentWillMount() {
+        this.readUser();
         this.readActivity();
+
     }
+
+
+wellwell(){
+    // let dos = this.state.items.map((boot,i) =>{
+    //     console.log(boot.companyName);
+    //     // let foc = boot.item.map((uno,i) =>{
+    //     //     console.log(uno.companyName);
+
+    //     // })
+    //     })
+      
+     Object.keys(this.state.items).map((item)=> {
+         console.log(item.map)
+     })
+
+        
+    }
+        
+
+
     readUser() {
         let userData = [];
         // let you = [];
         refUser.onSnapshot((doc) => {
-             userData.push(doc.data());
-               
+            userData.push(doc.data());
+
         })
 
-          this.setState({
-         
-            items:userData,
+        this.setState({
+            // ud:userData,
+            items: userData,
             loaded: true,
-            hrishi:true
-          })
-        }
+            hrishi: true
+        })
 
-        readActivity(){
-            let activityData = [];
-console.log("ref activity working")
-            refActivity.doc(`${this.props.userID}`).get().then((doc) => {
-                activityData.push(doc.data());
-                  
-           })
-   
-             this.setState({
-            
-               activity:activityData,
-               loaded: true,
-               hrishi:true
-             })
-           }
+       
+
+
+    }
+
+    readActivity() {
+        let activityData = [];
+        const refAct=refActivity.doc(`${this.props.userID}`);
         
-      
-    
+        refAct.onSnapshot((doc) => {
+            activityData.push(doc.data());
+            
+
+        })
+
+        this.setState({
+
+            activity: activityData,
+            loaded: true,
+            hrishi: true
+        })
+    }
+
+
+
+
+    // readActivity() {
+    //     let activityData = [];
+    //     const refAct=refActivity.doc(`${this.props.userID}`);
+        
+    //     refAct.onSnapshot((doc) => {
+    //         activityData.push(doc.data());
+            
+
+    //     })
+
+    //     this.setState({
+
+    //         activity: activityData,
+    //         loaded: true,
+    //         hrishi: true
+    //     })
+    // }
+
+
 
     render() {
         return (
             <div>
                 DocketEntry for {this.props.userID}
+                <button onClick={this.wellwell}>please</button>
 
             </div>
 
