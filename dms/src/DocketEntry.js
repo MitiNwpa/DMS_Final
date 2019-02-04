a7ef976fb2e9bc300bf4346491d7c28815e81854
HrishiGulhane <gulhane.hrishikesh@gmail.com>
2019-02-05 10:54:39 +1100

    init

--- dms/src/DocketEntry.js
+++ dms/src/DocketEntry.js
@@ -7,6 +7,7 @@ import './index.css'
 const db = firebase.firestore();
 const refUser = db.collection('user').doc('joe');
 const refActivity = db.collection('activity');
+const refDocket = db.collection('docket');
 
 
 
@@ -16,26 +17,30 @@ class DocketEntry extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            items:{},
-          companyName:'',
-          contactNumber : '',
-          firstName:'',
-          hourlyRate:'',
-          lastName:'',
-          overtimeRate:'',
-          position:'',
-          totalHours:'',
-          activityName:'',
-          ccNumber:'',
-          company:'',
-          id:'',
-          site:'',
-          
+            companyName: '',
+            contactNumber: '',
+            firstName: '',
+            hourlyRate: '',
+            lastName: '',
+            overtimeRate: '',
+            position: '',
+            totalHours: '',
+            activityName: '',
+            ccNumber: '',
+            company: '',
+            id: '',
+            site: '',
+            pay: ''
+
 
         }
         this.readUser = this.readUser.bind(this);
         this.readActivity = this.readActivity.bind(this);
-        this.writeUser = this.writeUser.bind(this);
+        this.calcHours = this.calcHours.bind(this);
+        this.addDocket = this.addDocket.bind(this);
+        // this.add = this.add.bind(this);
+
+
 
 
 
@@ -47,75 +52,72 @@ class DocketEntry extends Component {
         this.readUser();
         this.readActivity();
         
-      
+
+
 
     }
-    componentDidMount(){
-
-    }
 
 
-     
+
+    // readUser() {
+    //     let userData = [];
+    //     // let you = [];
+    //     refUser.onSnapshot((doc) => {
+    //         userData.push(doc.data());
+
+    //     })
+
+    //     this.setState({
+    //         items:userData,
+
+    //     })       
+
+
+    // }
 
 
     readUser() {
-        let userData = [];
-        // let you = [];
-        refUser.onSnapshot((doc) => {
-            userData.push(doc.data());
-            
-        })
-                   
-        this.setState({
-            items:userData,
-          
-        })       
-
-
-    }
-
-    writeUser(){
-        console.log("write user")
-       let dos= this.state.items.map((boot,i)=>{
-          
-        console.log(boot.companyName);
+        const promise = refUser.get();
+        promise.then((snapshot) => {
+            this.setState({
+                companyName: snapshot.data().companyName,
+                contactNumber: snapshot.data().contactNumber,
+                firstName: snapshot.data().firstName,
+                hourlyRate: snapshot.data().hourlyRate,
+                lastName: snapshot.data().lastName,
+                overtimeRate: snapshot.data().overtimeRate,
+                position: snapshot.data().position,
             })
-    
+        })
     }
 
     readActivity() {
-        let activityData = [];
-        const refAct=refActivity.doc(`${this.props.userID}`);
-        
-        refAct.onSnapshot((doc) => {
+        const promise = refActivity.doc(`${this.props.userID}`).get();
+        promise.then((snapshot) => {
 
-            // doc.forEach((item)=>{
-            //     console.log(item)
-            // })
-
-            activityData.push(doc.data());
-            
-
-        })
-
-        this.setState({
-
-            activity: activityData,
-            loaded: true,
-            hrishi: true
+            this.setState({
+                activityName: snapshot.data().activityName,
+                ccNumber: snapshot.data().ccNumber,
+                company: snapshot.data().company,
+                id: snapshot.data().id,
+                site: snapshot.data().site
+            })
         })
     }
-
-
 
 
     // readActivity() {
     //     let activityData = [];
     //     const refAct=refActivity.doc(`${this.props.userID}`);
-        
+
     //     refAct.onSnapshot((doc) => {
+
+    //         // doc.forEach((item)=>{
+    //         //     console.log(item)
+    //         // })
+
     //         activityData.push(doc.data());
-            
+
 
     //     })
 
@@ -129,11 +131,93 @@ class DocketEntry extends Component {
 
 
 
+
+    // readActivity() {
+    //     let activityData = [];
+    //     const refAct=refActivity.doc(`${this.props.userID}`);
+
+    //     refAct.onSnapshot((doc) => {
+    //         activityData.push(doc.data());
+
+
+    //     })
+
+    //     this.setState({
+
+    //         activity: activityData,
+    //         loaded: true,
+    //         hrishi: true
+    //     })
+    // }
+    updateInput = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+
+    }
+
+    calcHours() {
+        console.log('calc renderer')
+        let rate = this.state.hourlyRate;
+        let wHours = this.state.totalHours;
+        let pay = rate * wHours;
+
+        this.setState({
+            pay: pay
+        }, () =>{
+            this.addDocket()})
+        
+        // this.addDocket();
+
+    }
+
+
+    addDocket(){
+        // e.preventDefault();
+
+        const ref = refDocket.doc();
+        ref.set({
+            companyName: this.state.companyName,
+            contactNumber: this.state.contactNumber,
+            firstName: this.state.firstName,
+            hourlyRate: this.state.hourlyRate,
+            lastName: this.state.lastName,
+            overtimeRate: this.state.overtimeRate,
+            position: this.state.position,
+            totalHours: this.state.totalHours,
+            activityName: this.state.activityName,
+            ccNumber: this.state.ccNumber,
+            company: this.state.company,
+            id: ref.id,
+            site: this.state.site,
+            payAmount: this.state.pay
+        })
+    }
+    
+
+
+
+
+
+
+
     render() {
         return (
             <div>
                 DocketEntry for {this.props.userID}
-                <button onClick={this.writeUser}>please</button>
+
+                <input
+                    type="text"
+                    name="totalHours"
+                    placeholder='Enter Hours'
+                    onChange={this.updateInput}
+                    value={this.state.totalHours}
+                />
+                <Link to='/confirm'>
+                <button onClick={this.calcHours}>send</button>
+
+                </Link>
+                {/* <button onClick={this.addDocket}>Add docket</button> */}
 
             </div>
 
