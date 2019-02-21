ef4b03f3796eedc13bc96fef598023a57a9a7992
HrishiGulhane <gulhane.hrishikesh@gmail.com>
2019-02-21 14:14:07 +1100

    02

--- dms/src/pages/Dashboard.js
+++ dms/src/pages/Dashboard.js
@@ -6,26 +6,26 @@ const db = firebase.firestore();
 const refDoc = db.collection("docket");
 const refActivity = db.collection("activity");
 
-
 class Dashboard extends React.Component {
   constructor() {
     super();
     this.state = {
       sumArray: [],
-      ccArray:[],
-      sum: 0
+      ccArray: [],
+      sum: 0,
+      tempSum: 0,
+      tempArray:[]
     };
 
     this.calcSum = this.calcSum.bind(this);
     this.listCostCodes = this.listCostCodes.bind(this);
     this.mapCostCodes = this.mapCostCodes.bind(this);
-
-
+    this.clear = this.clear.bind(this);
   }
-  componentDidMount(){
+
+  componentDidMount() {
     this.calcSum();
     this.listCostCodes();
-
   }
 
   calcSum() {
@@ -46,55 +46,103 @@ class Dashboard extends React.Component {
         let finalSum = sumArray.reduce(function(a, b) {
           return a + b;
         }, 0);
+
         this.setState({
           sum: finalSum
         });
       });
   }
 
-
-
-
-  listCostCodes(){
+  listCostCodes() {
     let ccArray = [];
 
-    refActivity
-    .get()
-    .then(snapshot => {
+    refActivity.get().then(snapshot => {
       snapshot.forEach(doc => {
         ccArray.push(doc.data().ccNumber);
-      },()=>{
-        this.mapCostCodes();
       });
 
       this.setState({
         ccArray
       });
     });
-}
 
 
-mapCostCodes(){
-  const CostCodeMap = this.state.ccArray.map((index,item)=>{
-    console.log(`the cost code is ${index}`);
     return(
-      <div>
-      <li key={item.id} id={item.id}>the cost code is {index}</li>
-      </div>
+
+        this.mapCostCodes()
+    
     )
-  });
+  }
 
-  return(
-    <ul>
-      <div>
-        <h4>CostCode</h4>
+
+
+
+
+  clear() {
+    this.setState({
+      tempArray: 0,
+      tempSum: 0
+    });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  mapCostCodes() {
+    const CostCodeMap = this.state.ccArray.map((index, item) => {
+      let tempArray = [];
+
+      refDoc
+        .where("ccNumber", "==", `${index}`)
+        .get()
+        .then(snapshot => {
+          snapshot.forEach(doc => {
+            tempArray.push(doc.data().payAmount);
+          });
+
+          this.setState({
+            tempArray,
+            finalSum:[]
+          });
+
+          let finalSum = tempArray.reduce(function(a, b) {
+            return a + b;
+          }, 0);
+
+          this.setState({
+            tempSum: finalSum,
+            tempArray:[]
+          });
+        });
+
+      console.log(`the cost code is ${index}`);
+      return (
+        <div key={item.id} id={item.id}>
+          <li key={item.id}>
+            the cost code is {index} cost is {this.state.tempSum}
+          </li>
+        </div>
+      );
+    });
+
+    return (
+      <ul>
+        <div>
+          <h4>CostCode</h4>
           {CostCodeMap}
-      </div>
-    </ul>
-  )
-}
-
-  
+        </div>
+      </ul>
+    );
+  }
 
   render() {
     return (
@@ -104,7 +152,7 @@ mapCostCodes(){
           <h1>SUM :$ {this.state.sum}</h1>
 
           <p>This is the Dashboard 1234</p>
-          <div>{this.mapCostCodes()}</div>
+          <div>{this.listCostCodes()}</div>
         </div>
       </div>
     );
