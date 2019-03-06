9ab67f3027a54b1cee0aa83e74a592bfd9fff85e
HrishiGulhane <mitinwpa@gmail.com>
2019-03-06 11:58:33 +1100

    Updates

---
+++ precache-manifest.619e0ab980025ed3b5a3c5c919ef1b59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "504f4b3eadd53eecc12e",
+    "url": "/DMS_Final/static/js/runtime~main.504f4b3e.js"
+  },
+  {
+    "revision": "52ca62e67509365b7f6e",
+    "url": "/DMS_Final/static/js/main.52ca62e6.chunk.js"
+  },
+  {
+    "revision": "bbb262ea69bf0211f176",
+    "url": "/DMS_Final/static/js/2.bbb262ea.chunk.js"
+  },
+  {
+    "revision": "52ca62e67509365b7f6e",
+    "url": "/DMS_Final/static/css/main.c8ef8b01.chunk.css"
+  },
+  {
+    "revision": "cb90f1b3300b83f971b5773faec56bac",
+    "url": "/DMS_Final/index.html"
+  }
+];
